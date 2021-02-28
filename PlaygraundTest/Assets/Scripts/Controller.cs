e9770c3f6699ef72d1ba263c2216a15bdd533038
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-02-28 19:58:45 +0300

    VS setup, controller Init commit

---
+++ PlaygraundTest/Assets/Scripts/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    #region GemaLoop
+    private void Awake()
+    {
+        Matimatick();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+    #endregion
+
+    private void Matimatick()
+    {
+        var a = 5;
+        var b = 9;
+        var c = a;
+        c +=a + b;
+
+        Debug.Log($"{a}+{a}+{b}={c}");
+    }
+}
