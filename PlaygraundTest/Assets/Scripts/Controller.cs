aa9d9361254842eac76b4b22d8f33c65f217e328
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-03-06 11:55:52 +0300

    Massiv. Init commit

--- PlaygraundTest/Assets/Scripts/Controller.cs
+++ PlaygraundTest/Assets/Scripts/Controller.cs
@@ -8,6 +8,8 @@ public class Controller : MonoBehaviour
     {
         Matimatick();
         Loop();
+        Massiv();
+        Manymass();
     }
 
     private void OnEnable()
@@ -91,6 +93,45 @@ public class Controller : MonoBehaviour
             Debug.Log(b);
         }
 
+    }
+
+    private void Massiv()
+    {
+        int[] mas = new int[5];
+        mas[0] = 44;
+        mas[1] = 999;
+        mas[2] = 1111;
+        mas[3] = 33333;
+        mas[4] = 1;
+        Debug.Log(mas[2]);
+
+        Debug.Log("Перебор массива с foreach");
+        foreach (int i in mas)
+        {
+            Debug.Log(i);
+        }
+
+        Debug.Log("Перебор массива с for");
+        for (int i = 0; i < mas.Length; i++)
+        {
+            Debug.Log(mas[i]);
+        }
+    }
+
+    private void Manymass()
+    {
+        int[,] mas = new int[2, 4] { { 44, 55, 66, 77 }, { 11, 22, 33, 88, } };
+        foreach (int i in mas)
+        {
+            Debug.Log(i);
+        }
+
+        int[,] mMasiv = { { 14, 13, 12, 11, 10 }, { 9, 8, 7, 6, 5 }, { 4, 3, 2, 1, 0 } };
+        foreach(int i in mMasiv)
+        {
+            Debug.Log(i);
+        }
+        
     }
     #endregion
 }
