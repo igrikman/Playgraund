fefd74620d40b6c6897962cbc15a93520a512a60
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-04-06 18:34:55 +0300

    Enemy

---
+++ PlaygraundTest/Assets/Scripts/Sphere/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    public GameObject player;
+
+    private void OnCollisionEnter(Collision EnemyCol)
+    {
+        if (EnemyCol.gameObject.tag == "Player")
+        {
+            Destroy(player, 0f);
+        }
+    }
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
