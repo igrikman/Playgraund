a8083fd8bdecd21e1be239809cc8beba0cd39fbe
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-05-05 13:32:21 +0300

    Enemis ver 0.00001

---
+++ PlaygraundTest/Assets/Scripts/EnemyRino.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyRino : MonoBehaviour
+{
+    [SerializeField] private float speed = 11.0f;
+
+    [SerializeField] private Rigidbody enemyRb;
+
+    [SerializeField] private Transform player;
+    
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+      
+    }
+    public void Die() => Destroy(gameObject);
+}
