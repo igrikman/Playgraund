3a61f3480399b45e8e73b9e718919c200ab23d8a
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-05-06 12:56:02 +0300

    Anim : enemy movement

--- PlaygraundTest/Assets/Scripts/EnemyRino.cs
+++ PlaygraundTest/Assets/Scripts/EnemyRino.cs
@@ -10,15 +10,22 @@ public class EnemyRino : MonoBehaviour
     [SerializeField] private Rigidbody enemyRb;
 
     [SerializeField] private Transform player;
-    
+    [SerializeField] private NavMeshAgent navmesh;
+
+
 
     void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        navmesh = GetComponent<NavMeshAgent>();
     }
     void Update()
     {
-      
+        navmesh.destination = player.transform.position;
+        //if  (gameObject.transform.position != player.transform.position)
+        //{
+        //    GetComponent<Animator>().Play("Walk");
+        //}
     }
-    public void Die() => Destroy(gameObject);
+    
 }
