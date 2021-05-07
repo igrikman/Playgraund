6b5ce57a4513a27072c14540f0749df52020a882
DESKTOP-PSPE6EH\lider <lider.x2011@yandex.ru>
2021-05-07 13:36:28 +0300

    My anim fack rino

--- PlaygraundTest/Assets/Scripts/EnemyRino.cs
+++ PlaygraundTest/Assets/Scripts/EnemyRino.cs
@@ -5,27 +5,72 @@ using UnityEngine.AI;
 
 public class EnemyRino : MonoBehaviour
 {
+    private Animator myAnimator;
+    private float distance;
+    
+
     [SerializeField] private float speed = 11.0f;
-
     [SerializeField] private Rigidbody enemyRb;
-
     [SerializeField] private Transform player;
     [SerializeField] private NavMeshAgent navmesh;
-
-
-
-    void Awake()
+    private void Start()
     {
-        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        myAnimator = GetComponent<Animator>();
         navmesh = GetComponent<NavMeshAgent>();
     }
+    //private void OnCollisionEnter(Collision RinoCol)
+    //{
+    //    if (RinoCol.gameObject.tag == "Player")
+    //    {
+    //        GetComponent<Animator>().Play("Attack");
+    //        Debug.LogError("Атакую");
+    //    }
+    //}
+
+    //void Awake()
+    //{
+    //    //player = GameObject.FindGameObjectWithTag("Player").transform;
+    //    navmesh = GetComponent<NavMeshAgent>();
+    //}
     void Update()
     {
-        navmesh.destination = player.transform.position;
-        //if  (gameObject.transform.position != player.transform.position)
+        distance = Vector3.Distance(player.position, transform.position);
+
+        //if(distance > 20)
+        //{
+        //    transform.LookAt(player.position);
+        //    navmesh.enabled = false;
+        //    myAnimator.SetBool("Walk",false);
+        //    myAnimator.SetBool("Attack", false);
+
+        //}
+        if (distance <= 8 & distance >1.5f)
+        {    
+            navmesh.enabled = true;
+            navmesh.SetDestination(player.position);
+            myAnimator.SetBool("Walk",true);
+            myAnimator.SetBool("Attack", false);
+
+        }
+        if (distance <= 1.5f)
+        {
+            navmesh.enabled = false;
+            myAnimator.SetBool("Attack",true);
+            myAnimator.SetBool("Walk", false);
+
+        }
+
+
+        //navmesh.destination = player.transform.position;
+        //if (gameObject.transform.position != player.transform.position)
         //{
         //    GetComponent<Animator>().Play("Walk");
         //}
+        //else
+        //{
+        //    navmesh.enabled = false;
+        //    Debug.LogError("Не иду");
+        //}
     }
-    
+
 }
